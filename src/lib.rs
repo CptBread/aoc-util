a67eb88245f9108087b26ac258b720147fa6d3d4
CptBread <fredrik.astrom89@gmail.com>
2022-11-27 15:50:11 +0100

    * Fixed warnings and some more cleanup. * Create Trim and removed SeperatedTrim. * Vec adaptors can now take other adaptors(e.g. Trim)

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-use std::str::FromStr;
+use std::str::{FromStr};
 
 #[macro_export]
 macro_rules! parse_util{
 	($s:ident, $t:ty, $end:literal) => {
 		{
 			let f = |s: &str| {
-				let mut ss = s;
-				Some(parse_util!(step ss, $t, $end))
+				// _ss so that we don't get faulty "value assigned to `ss` is never read" warnings
+				let mut _ss = s;
+				Some(parse_util!(step _ss, $t, $end))
 			};
 			f($s)
 		}
@@ -35,40 +36,14 @@ macro_rules! parse_util{
 	($s:ident $(, $more_t:ty, $more_s:literal)*) => {
 		{
 			let f = |s: &str| {
-				let mut ss = s;
-				Some(($(parse_util!(step ss, $more_t, $more_s),)*))
+				// _ss so that we don't get faulty "value assigned to `ss` is never read" warnings
+				let mut _ss = s;
+				Some(($(parse_util!(step _ss, $more_t, $more_s),)*))
 			};
 			f($s)
 		}
 	};
 
-	(step $s:ident, $t:ty, $end:literal) => {
-		if $end == "" {
-			if let Some(v) = <$t>::parse($s) {
-				$s = "";
-				v
-			}
-			else {
-				return None;
-			}
-		}
-		else if let Some((front, end)) = $s.split_once($end) {
-			$s = end;
-			if let Some(v) = <$t>::parse(front) {
-				v
-			}
-			else {
-				return None;
-			}
-		}
-		else
-		{
-			return None;
-		}
-	};
-}
-
-macro_rules! parse_step {
 	(step $s:ident, $t:ty, $end:literal) => {
 		if $end == "" {
 			if let Some(v) = <$t>::parse($s) {
@@ -107,25 +82,15 @@ impl<T> ParseUtil for T where T: FromStr {
 	}
 }
 
-pub struct Csv<T>(Vec<T>);
+pub type Csv<T> = CsvStict<Trim<T>>;
 
-impl<T> ParseUtil for Csv<T> where T: ParseUtil{
-	type Res = Vec<T::Res>;
-	fn parse(s: &str) -> Option<Self::Res> {
-		let mut res = Vec::new();
-		for s in s.split(",") {
-			res.push(T::parse(s.trim())?);
-		}
-		Some(res)
-	}
-}
-
-pub struct CsvStict<T>(Vec<T>);
+pub struct CsvStict<T>(Vec<<T as ParseUtil>::Res>) where T: ParseUtil;
 
 impl<T> ParseUtil for CsvStict<T> where T: ParseUtil{
 	type Res = Vec<T::Res>;
 	fn parse(s: &str) -> Option<Self::Res> {
 		let mut res = Vec::new();
+		// TODO: Properly do csv parsing
 		for s in s.split(",") {
 			res.push(T::parse(s)?);
 		}
@@ -133,7 +98,7 @@ impl<T> ParseUtil for CsvStict<T> where T: ParseUtil{
 	}
 }
 
-pub struct Seperated<T, const SEP: char>(Vec<T>);
+pub struct Seperated<T, const SEP: char>(Vec<<T as ParseUtil>::Res>) where T: ParseUtil;
 
 impl<T, const SEP: char> ParseUtil for Seperated<T, SEP> where T: ParseUtil{
 	type Res = Vec<T::Res>;
@@ -146,16 +111,11 @@ impl<T, const SEP: char> ParseUtil for Seperated<T, SEP> where T: ParseUtil{
 	}
 }
 
-pub struct SeperatedTrim<T, const SEP: char>(Vec<T>);
-
-impl<T, const SEP: char> ParseUtil for SeperatedTrim<T, SEP> where T: ParseUtil{
-	type Res = Vec<T::Res>;
+pub struct Trim<T>(<T as ParseUtil>::Res) where T: ParseUtil;
+impl<T> ParseUtil for Trim<T> where T: ParseUtil {
+	type Res = <T as ParseUtil>::Res;
 	fn parse(s: &str) -> Option<Self::Res> {
-		let mut res = Vec::new();
-		for s in s.split(SEP) {
-			res.push(T::parse(s.trim())?);
-		}
-		Some(res)
+		<T>::parse(s.trim())
 	}
 }
 
@@ -174,12 +134,17 @@ mod tests {
 	}
 
 	#[test]
+	fn parse_trim() {
+		assert_eq!(parse_util!("test 12", "test", Trim<u8>, ""), Some(12));
+	}
+
+	#[test]
 	fn parse_csv() {
 		assert_eq!(parse_util!("test 12, 11,10;", "test ", Csv<u32>, ";"), Some(vec![12, 11, 10]));
 	}
 
 	#[test]
 	fn parse_seperator() {
-		assert_eq!(parse_util!("test 12. 11.10;", "test ", SeperatedTrim<u32, '.'>, ";"), Some(vec![12, 11, 10]));
+		assert_eq!(parse_util!("test 12. 11.10;", "test ", Seperated<Trim<u32>, '.'>, ";"), Some(vec![12, 11, 10]));
 	}
 }
