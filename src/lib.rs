4d04ef5ade006f8e575fce1c6caeddf898634d08
CptBread <fredrik.astrom89@gmail.com>
2022-11-28 18:45:15 +0100

    First version that works with &str

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,15 @@
 use std::str::{FromStr};
+use core::marker::PhantomData;
 
 #[macro_export]
 macro_rules! parse_util{
 	($s:ident, $t:ty, $end:literal) => {
 		{
+			// Create a lambda to be called so that we can shortcirtuit at any point
 			let f = |s: &str| {
-				// _ss so that we don't get faulty "value assigned to `ss` is never read" warnings
-				let mut _ss = s;
-				Some(parse_util!(step _ss, $t, $end))
+				#[allow(unused)]
+				let mut ss = s;
+				Some(parse_util!(step ss, $t, $end))
 			};
 			f($s)
 		}
@@ -19,13 +21,15 @@ macro_rules! parse_util{
 
 	($s:literal $(,$prefix:literal)? $(, $more_t:ty, $more_s:literal)*) => {
 		{
-			let ss = $s;
+			#[allow(unused)]
+			let mut ss = $s;
 			parse_util!(ss $(,$prefix)? $(, $more_t, $more_s)*)
 		}
 	};
 
 	($s:ident, $prefix:literal $(, $more_t:ty, $more_s:literal)*) => {
-		if let Some(rest) = $s.strip_prefix($prefix) {
+		#[allow(unused)]
+		if let Some(mut rest) = $s.strip_prefix($prefix) {
 			parse_util!(rest $(, $more_t, $more_s)*)
 		}
 		else {
@@ -35,18 +39,18 @@ macro_rules! parse_util{
 
 	($s:ident $(, $more_t:ty, $more_s:literal)*) => {
 		{
-			let f = |s: &str| {
+			let mut f = || {
 				// _ss so that we don't get faulty "value assigned to `ss` is never read" warnings
-				let mut _ss = s;
-				Some(($(parse_util!(step _ss, $more_t, $more_s),)*))
+				// let mut _ss = s;
+				Some(($(parse_util!(step $s, $more_t, $more_s),)*))
 			};
-			f($s)
+			f()
 		}
 	};
 
 	(step $s:ident, $t:ty, $end:literal) => {
 		if $end == "" {
-			if let Some(v) = <$t>::parse($s) {
+			if let Some(v) = <$t>::long_name_for_macro_calling_parse($s) {
 				$s = "";
 				v
 			}
@@ -56,7 +60,7 @@ macro_rules! parse_util{
 		}
 		else if let Some((front, end)) = $s.split_once($end) {
 			$s = end;
-			if let Some(v) = <$t>::parse(front) {
+			if let Some(v) = <$t>::long_name_for_macro_calling_parse(front) {
 				v
 			}
 			else {
@@ -73,6 +77,10 @@ macro_rules! parse_util{
 pub trait ParseUtil {
 	type Res;
 	fn parse(s: &str) -> Option<Self::Res>;
+
+	fn long_name_for_macro_calling_parse(s: &str) -> Option<Self::Res> {
+		Self::parse(s)
+	}
 }
 
 impl<T> ParseUtil for T where T: FromStr {
@@ -82,9 +90,24 @@ impl<T> ParseUtil for T where T: FromStr {
 	}
 }
 
+// Seperate trait as because we are using macros so lets use that to deal with pass through &str
+pub trait ParseUtilStr<'a> {
+	fn parse(s: &'a str) -> Option<&'a str>;
+
+	fn long_name_for_macro_calling_parse(s: &'a str) -> Option<&'a str> {
+		Self::parse(s)
+	}
+}
+
+impl<'a> ParseUtilStr<'a> for &'a str {
+	fn parse(s: &'a str) -> Option<&'a str> {
+		Some(s)
+	}
+}
+
 pub type Csv<T> = CsvStict<Trim<T>>;
 
-pub struct CsvStict<T>(Vec<<T as ParseUtil>::Res>) where T: ParseUtil;
+pub struct CsvStict<T>(PhantomData<T>);
 
 impl<T> ParseUtil for CsvStict<T> where T: ParseUtil{
 	type Res = Vec<T::Res>;
@@ -98,7 +121,7 @@ impl<T> ParseUtil for CsvStict<T> where T: ParseUtil{
 	}
 }
 
-pub struct Seperated<T, const SEP: char>(Vec<<T as ParseUtil>::Res>) where T: ParseUtil;
+pub struct Seperated<T, const SEP: char>(PhantomData<T>);
 
 impl<T, const SEP: char> ParseUtil for Seperated<T, SEP> where T: ParseUtil{
 	type Res = Vec<T::Res>;
@@ -111,10 +134,16 @@ impl<T, const SEP: char> ParseUtil for Seperated<T, SEP> where T: ParseUtil{
 	}
 }
 
-pub struct Trim<T>(<T as ParseUtil>::Res) where T: ParseUtil;
+pub struct Trim<T>(PhantomData<T>);
 impl<T> ParseUtil for Trim<T> where T: ParseUtil {
 	type Res = <T as ParseUtil>::Res;
 	fn parse(s: &str) -> Option<Self::Res> {
+		<T>::parse(s.trim())
+	}
+}
+
+impl<'a, T> ParseUtilStr<'a> for Trim<T> where T: ParseUtilStr<'a> {
+	fn parse(s: &'a str) -> Option<&'a str> {
 		<T>::parse(s.trim())
 	}
 }
@@ -126,6 +155,11 @@ mod tests {
 	#[test]
 	fn parse_many() {
 		assert_eq!(parse_util!("test 1 2 3 h all", "test ", u8, " ", u8, " ", u8, " ", char, " ", String, ""), Some((1, 2, 3, 'h', "all".into())));
+	}
+
+	#[test]
+	fn parse_str() {
+		assert_eq!(parse_util!("test 1 2 3 h all", "test ", u8, " ", u8, " ", u8, " ", char, " ", &str, ""), Some((1, 2, 3, 'h', "all")));
 	}
 
 	#[test]
